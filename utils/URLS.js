c6baaa80ef48f722a0451ca0361d0c62c43c5d64
Pranav Ajay <86167689+MoeZilla@users.noreply.github.com>
2021-12-06 16:52:59 +0530

    Create URLS.js

---
+++ utils/URLS.js
@@ -0,0 +1,9 @@
+export const URL = {
+    POPULAR: process.env.BASE_URL + "popular/",
+    RECENT: process.env.BASE_URL + "recentlyadded/",
+    DETAILS: process.env.BASE_URL + "details/",
+    EPLINK: process.env.BASE_URL + "watching/",
+    GENRES: process.env.BASE_URL + "genre/",
+    SEARCH: process.env.BASE_URL + "search/",
+    NEWSEASON: process.env.BASE_URL + "newseason/",
+};
