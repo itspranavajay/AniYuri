963cb7d3356070e3f3b630de31c35febd6384a94
Pranav Ajay <86167689+MoeZilla@users.noreply.github.com>
2021-12-06 16:53:44 +0530

    Create data.js

---
+++ utils/data.js
@@ -0,0 +1,93 @@
+import {
+    AiFillHeart,
+    AiFillCalendar,
+    AiFillPlayCircle,
+    AiFillStar,
+} from "react-icons/ai";
+
+export const Discover = [{
+        index: 1,
+        name: "Recently Added",
+        link: "/recentlyadded/1",
+        icon: AiFillCalendar,
+    },
+    {
+        index: 0,
+        name: "Popular",
+        link: "/popular/1",
+        icon: AiFillStar,
+    },
+    {
+        index: 2,
+        name: "New Season",
+        link: "/newseason/1",
+        icon: AiFillStar,
+    },
+    {
+        index: 3,
+        name: "My List",
+        link: "/myList",
+        icon: AiFillHeart,
+    },
+];
+
+export const About = [{
+        index: 1,
+        name: "Support",
+        link: "https://t.me/aniyurisupport",
+        icon: AiFillStar,
+    },
+];
+
+
+const genrelist = [
+    "Action",
+    "Adventure",
+    "Cars",
+    "Comedy",
+    "Dementia",
+    "Demons",
+    "Drama",
+    "Dub",
+    "Ecchi",
+    "Fantasy",
+    "Game",
+    "Harem",
+    "Historical",
+    "Horror",
+    "Josei",
+    "Kids",
+    "Magic",
+    "Martial Arts",
+    "Mecha",
+    "Military",
+    "Music",
+    "Mystery",
+    "Parody",
+    "Police",
+    "Psychological",
+    "Romance",
+    "Samurai",
+    "School",
+    "Sci-Fi",
+    "Seinen",
+    "Shoujo",
+    "Shoujo Ai",
+    "Shounen",
+    "Shounen Ai",
+    "Slice of Life",
+    "Space",
+    "Sports",
+    "Super Power",
+    "Supernatural",
+    "Thriller",
+    "Vampire",
+    "Yaoi",
+    "Yuri",
+];
+export var Genre = genrelist.map((name, index) => ({
+    index: index,
+    name: name,
+    link: `/genre/${name.split(" ").join("-")}/1`,
+    icon: AiFillPlayCircle,
+}));
