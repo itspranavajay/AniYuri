56237141d62f73c3e6b34bda7650e470b2ba14a4
Pranav Ajay <86167689+MoeZilla@users.noreply.github.com>
2021-12-10 16:34:40 +0530

    Update URLS.js

--- utils/URLS.js
+++ utils/URLS.js
@@ -2,7 +2,7 @@ export const URL = {
     POPULAR: "https://aniyuriapi.herokuapp.com/api/" + "popular/",
     RECENTSUB: "https://aniyuriapi.herokuapp.com/api/" + "recentlysub/",
     RECENTDUB: "https://aniyuriapi.herokuapp.com/api/" + "recentlydub/",
-    DETAILS: "https://aniyuriapi.herokuapp.com/api/" + "details/",
+    DETAILS: "https://aniyuriapi.vercel.app/api/" + "details/",
     EPLINK: "https://aniyuriapi.herokuapp.com/api/" + "watching/",
     GENRES: "https://aniyuriapi.herokuapp.com/api/" + "genre/",
     SEARCH: "https://aniyuriapi.herokuapp.com/api/" + "search/",
